5f4f769fe549ee9b7f6542ed693b709f192c6418
antho <anthonynuge0509@gmail.com>
2025-02-27 21:01:34 -0600

    Add toast for notifications and successful delete.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import { useState } from 'react';
-import { Outlet, useNavigation, useParams } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { Outlet, useNavigation, useParams, useActionData } from 'react-router-dom';
 
 import PageTitle from './components/PageTitle';
 import NavBar from './components/NavBar';
 import SidePanel from './components/SidePanel';
 import Welcome from './pages/Welcome';
 import ChatInput from './components/ChatInput';
+import usePromptPreloader from './hooks/usePromptPreloader';
+import { toast } from "react-toastify"
 
 const App = () => {
   const [sidePanelOpen, setSidePanelOpen] = useState(false);
 
   const param = useParams()
   const navigation = useNavigation();
+  const resultData = useActionData();
+  const histRef = useRef<HTMLDivElement | null>(null);
+  const { preLoaderVal } = usePromptPreloader();
+
+  // Confirmation message if chat deleted successfully
+  useEffect(() => {
+    if (resultData?.chatTitle) {
+      toast.success(`Success: Chat ${resultData.chatTitle} deleted!`)
+    }
+  }, [resultData])
+
+  useEffect(() => {
+    const hist = histRef.current;
+    if (preLoaderVal) {
+      hist?.scroll({
+        top: hist.scrollHeight - hist.clientHeight,
+        behavior: "smooth"
+      })
+    }
+  }, [histRef, preLoaderVal])
 
   const toggleSidePanel = () => {
     setSidePanelOpen((prev) => !prev);
@@ -35,7 +57,9 @@ const App = () => {
           <NavBar toggleSidePanel={toggleSidePanel} />
 
           {/* Response area area */}
-          <div className="px-4 pb-4 flex flex-col overflow-y-auto mb-6">
+          <div className="px-4 pb-4 flex flex-col overflow-y-auto mb-6"
+            ref={histRef}
+          >
             <div className="max-w-[830px] w-full mx-auto h-full">
               {isPageLoad ? null : param.chatId ? (
                 <Outlet />
--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,21 @@ import { RouterProvider } from "react-router-dom";
  */
 import router from "./routers/routes.tsx";
 import "./index.css";
+import { ToastContainer } from "react-toastify"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer
+      position="bottom-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      pauseOnHover
+      theme="dark"
+    />
   </StrictMode>
 );
