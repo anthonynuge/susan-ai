b8a4f260ddbe33d2877b6928b8cd7357ca7a87b7
antho <anthonynuge0509@gmail.com>
2025-03-01 01:54:57 -0600

    Refactor submit prompt.

--- src/routers/actions/submitPrompt.tsx
+++ src/routers/actions/submitPrompt.tsx
@@ -1,12 +1,19 @@
 import { redirect } from 'react-router-dom';
 
-import { getConvoTitle, getGeminiResponse } from '../../api/geminiAI.ts';
-import { account, databases } from '../../lib/appwrite.ts'
+import { getConvoTitle, getGeminiResponse } from '@/api/geminiAI.ts';
+import { account, databases } from '@/lib/appwrite.ts'
 import { ID } from "appwrite"
 
-const chatDeleteAction = async (formData) => {
-  const chatId = formData.get('chat_id');
-  const chatTitle = formData.get('chat_title');
+
+const chatDeleteAction = async (formData: FormData) => {
+  const chatId = formData.get('chat_id') as string | null;
+  const chatTitle = formData.get('chat_title') as string | null;
+
+  if (!chatId) {
+    console.error("Missing ChatId.")
+    return { error: "ChatId is required" };
+  }
+
   try {
     await databases.deleteDocument(
       import.meta.env.VITE_APPWRITE_DATABASE_ID,
@@ -17,21 +24,27 @@ const chatDeleteAction = async (formData) => {
 
   } catch (error) {
     if (error instanceof Error) {
-      console.log(`Error deleting chat. ${error.message}`)
+      console.error(`Error deleting chat. ${error.message}`)
+      return { error: "Failed to delete the chat" }
     }
   }
 }
 
-const userPromptAction = async (formData) => {
-  const prompt = formData.get('user_prompt')
+const userPromptAction = async (formData: FormData) => {
+  const prompt = formData.get('user_prompt') as string | null;
 
-  // get the users data
-  const user = await account.get();
-  const title = await getConvoTitle(prompt);
+  if (!prompt) {
+    return { error: "Prompt is required" }
+  }
+
 
   let chat = null;
 
   try {
+    // get the users data
+    const user = await account.get();
+    const title = await getConvoTitle(prompt);
+
     chat = await databases.createDocument(
       import.meta.env.VITE_APPWRITE_DATABASE_ID,
       'chats',
@@ -73,18 +86,25 @@ const userPromptAction = async (formData) => {
   }
 }
 
-
 // Async function that does different things depending on the request
 // returns Promise based on the type
 const submitPrompt = async ({ request }: { request: Request }) => {
   const formData = await request.formData();
-  const requestType = formData.get('request_type')
+  const requestType = formData.get('request_type') as string | null;
+
+  if (!requestType) {
+    console.warn("Request type is missing.");
+    return { error: "Request type is required" };
+  }
 
   if (requestType === 'user_prompt') {
     return await userPromptAction(formData);
   } else if (requestType === 'delete_chat') {
     return await chatDeleteAction(formData);
   }
+
+  console.warn(`Unknown request type ${requestType}`);
+  return { error: "Invalid request type" };
 }
 
 export default submitPrompt
